35c3491a36d3d9706d9058a9da45fa3484eb20a7
titho93 <titho93.10@gmail.com>
2023-10-19 15:51:56 +0200

    [add]jumbotron

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -7,9 +7,51 @@ export default {
 </script>
 
 <template>
+
+  <div class="container-jb">
+
+    <img src="/home1_slide_three_bg_2.jpg" alt="">
+
+    <div class="description">
+
+      <h1>Brand New Arrivals</h1>
+      <h4>NEW COLLECTION FROM NEW YORK</h4>
+
+      <div class="buttons">
+        <button>VIEW ALL</button>
+        <button>LOOKBOOK</button>
+      </div>
+
+    </div>
+
+  </div>
   
 </template>
 
 <style lang="scss">
 
+@use '../scss/Main.scss';
+
+.container-jb {
+  width: 100vw;
+  height: 650px;
+  position: relative;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  .description {
+    position: absolute;
+    top: 185px;
+    left: 550px;
+    text-align: center;
+    .buttons {
+      padding-top: 20px;
+      button {
+        margin: 8px;
+      }
+    }
+  }
+}
 </style>
