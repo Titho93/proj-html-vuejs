cd64528a6d6bd10ee94fff89c53626241830809e
titho93 <titho93.10@gmail.com>
2023-10-22 11:33:20 +0200

    [edit]dinamic Recap

--- src/components/partials/Recap.vue
+++ src/components/partials/Recap.vue
@@ -1,198 +1,134 @@
 <script>
 
 export default {
-  name: 'Recap'  
+  name: 'Recap',
+  data () {
+    return {
+        squares: [
+            {
+                title: 'FEATURED',
+                products: [
+                    {
+                        title: 'Black Jacket',
+                        discount: '',
+                        price: '$125',
+                        image: 'black_leather_jacket-400x520.jpg'
+                    },
+                    {
+                        title: 'Black Leather Jacket',
+                        rate: 'fa-solid fa-star',
+                        discount: '$235',
+                        price: '$200',
+                        image: 'black_elegant_leather_jacket-400x520.jpg'
+                    },
+                    {
+                        title: 'Hipster Black Top',
+                        discount: '',
+                        price: '$57',
+                        image: 'hipster_black_top-400x520.jpg'
+                    },
+                ]
+            },
+            {
+                title: 'ON SALE',
+                products: [
+                    {
+                        title: 'Black Leather Jacket',
+                        rate: 'fa-solid fa-star',
+                        discount: '$235',
+                        price: '$200',
+                        image: 'black_elegant_leather_jacket-400x520.jpg'
+                    },
+                    {
+                        title: 'Blue Leather Jacket',
+                        discount: '$80',
+                        price: '$60',
+                        image: 'blue_leather_jacket-400x520.jpg'
+                    },
+                    {
+                        title: 'Modern Leather Boots',
+                        discount: '$50',
+                        price: '$30',
+                        image: 'modern_leather_boots-400x520.jpg'
+                    },
+                ]
+            },
+            {
+                title: 'TOP RATED',
+                products: [
+                    {
+                        title: 'Leather Gloves',
+                        rate: 'fa-solid fa-star',
+                        price: '$45',
+                        image: 'leather_gloves-400x520.jpg'
+                    },
+                    {
+                        title: 'Black Leather Jacket',
+                        rate: 'fa-solid fa-star',
+                        discount: '$235',
+                        price: '$200',
+                        image: 'black_elegant_leather_jacket-400x520.jpg'
+                    },
+                    {
+                        title: 'Spring Printed Dress',
+                        rate: 'fa-solid fa-star',
+                        price: '$47',
+                        image: 'spring_printed_dress-400x520.jpg'
+                    },
+                ]
+            },
+            {
+                title: 'LATEST REVIEWS',
+                products: [
+                    {
+                        title: 'Black Leather Jacket',
+                        rate: 'fa-solid fa-star',
+                        price: 'by admin',
+                        image: 'black_elegant_leather_jacket-400x520.jpg'
+                    },
+                    {
+                        title: 'Leather Gloves',
+                        rate: 'fa-solid fa-star',
+                        price: 'by Beardman',
+                        image: 'leather_gloves-400x520.jpg'
+                    },
+                    {
+                        title: 'Spring Printed Dress',
+                        rate: 'fa-solid fa-star',
+                        price: 'by admin',
+                        image: 'spring_printed_dress-400x520.jpg'
+                    },
+                ]
+            }
+        ]
+    }
+  }  
 }
 </script>
 
 <template>
   <div class="container-rc">
       <div class="recap d-flex">
-        <ul>
-            <span class="title">FEATURED</span>
-            <li class="d-flex">
+        <ul v-for="(square,index) in squares"
+        :key="index">
+            <span class="title">{{ square.title }}</span>
+            <li v-for="(product,index) in square.products" class="d-flex"
+            :key="index">
                 <div class="bio d-flex">
-                    <span class="object">Black Jacket</span>
-                    <div class="price">
-                        <span class="real">$125</span>
-                    </div>
-                </div>
-                <img src="../../assets/black_leather_jacket-400x520.jpg" alt="">
-            </li>
-            <li class="d-flex">
-                <div class="bio d-flex">
-                    <span class="object">Black Leather Jacket</span>
+                    <span class="object">{{ product.title }}</span>
                     <span class="stars">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                    </span>
+                                <i v-for="index in 5"
+                                :key="index"
+                                :class="`${ product.rate }`"></i>
+                            </span>
                     <div class="price">
-                        <span class="discount">$235</span>
-                        <span class="real">$200</span>
+                        <span class="discount">{{ product.discount }}</span>
+                        <span class="real">{{ product.price }}</span>
                     </div>
                 </div>
-                <img src="../../assets/black_elegant_leather_jacket-400x520.jpg" alt="">
+                <img :src="`../../assets/${ product.image }`" alt="">
             </li>
-            <li class="d-flex">
-                <div class="bio d-flex">
-                    <span class="object">Hipster Black Top</span>
-                    <div class="price">
-                        <span class="real">$57</span>
-                    </div>
-                </div>
-                <img src="../../assets/hipster_black_top-400x520.jpg" alt="">
-            </li>
-
-        </ul>
-        <ul>
-            <span class="title">ON SALE</span>
-            <li class="d-flex">
-                <div class="bio d-flex">
-                    <span class="object">Black Leather Jacket</span>
-                    <span class="stars">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                    </span>
-                    <div class="price">
-                        <span class="discount">$235</span>
-                        <span class="real">$200</span>
-                    </div>
-                </div>
-                <img src="../../assets/black_elegant_leather_jacket-400x520.jpg" alt="">
-            </li>
-            <li class="d-flex">
-                <div class="bio d-flex">
-                    <span class="object">Blue Leather Jacket</span>
-                    <div class="price">
-                        <span class="discount">$80</span>
-                        <span class="real">$60</span>
-                    </div>
-                </div>
-                <img src="../../assets/blue_leather_jacket-400x520.jpg" alt="">
-            </li>
-            <li class="d-flex">
-                <div class="bio d-flex">
-                    <span class="object">Modern Leather Boots</span>
-                    <div class="price">
-                        <span class="discount">$50</span>
-                        <span class="real">$30</span>
-                    </div>
-                </div>
-                <img src="../../assets/modern_leather_boots-400x520.jpg" alt="">
-            </li>
-
-        </ul>
-        <ul>
-            <span class="title">TOP RATED</span>
-            <li class="d-flex">
-                <div class="bio d-flex">
-                    <span class="object">Leather Gloves</span>
-                    <span class="stars">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                    </span>
-                    <div class="price">
-                        <span class="real">$45</span>
-                    </div>
-                </div>
-                <img src="../../assets/leather_gloves-400x520.jpg" alt="">
-            </li>
-            <li class="d-flex">
-                <div class="bio d-flex">
-                    <span class="object">Black Leather Jacket</span>
-                    <span class="stars">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                    </span>
-                    <div class="price">
-                        <span class="discount">$235</span>
-                        <span class="real">$200</span>
-                    </div>
-                </div>
-                <img src="../../assets/black_elegant_leather_jacket-400x520.jpg" alt="">
-            </li>
-            <li class="d-flex">
-                <div class="bio d-flex">
-                    <span class="object">Spring Printed Dress</span>
-                    <span class="stars">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                    </span>
-                    <div class="price">
-                        <span class="real">$47</span>
-                    </div>
-                </div>
-                <img src="../../assets/spring_printed_dress-400x520.jpg" alt="">
-            </li>
-
-        </ul>
-        <ul>
-            <span class="title">LATEST REVIEWS</span>
-            <li class="d-flex">
-                <div class="bio d-flex">
-                    <span class="object">Black Leather Jacket</span>
-                    <span class="stars">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                    </span>
-                    <div class="price">
-                        <span class="admint">by admin</span>
-                    </div>
-                </div>
-                <img src="../../assets/black_elegant_leather_jacket-400x520.jpg" alt="">
-            </li>
-            <li class="d-flex">
-                <div class="bio d-flex">
-                    <span class="object">Leather Gloves</span>
-                    <span class="stars">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                    </span>
-                    <div class="price">
-                        <span class="admint">by Beardman</span>
-                    </div>
-                </div>
-                <img src="../../assets/leather_gloves-400x520.jpg" alt="">
-            </li>
-            <li class="d-flex">
-                <div class="bio d-flex">
-                    <span class="object">Spring Printed Dress</span>
-                    <span class="stars">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                    </span>
-                    <div class="price">
-                        <span class="admint">by admin</span>
-                    </div>
-                </div>
-                <img src="../../assets/spring_printed_dress-400x520.jpg" alt="">
-            </li>
-
-        </ul>
+        </ul>    
  
       </div>
 
