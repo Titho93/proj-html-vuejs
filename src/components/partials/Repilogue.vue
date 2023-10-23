9069996c4a76b61c17a638a0653116770edb0548
titho93 <titho93.10@gmail.com>
2023-10-23 11:41:44 +0200

    [edit]toprated

--- src/components/partials/Repilogue.vue
+++ src/components/partials/Repilogue.vue
@@ -1,33 +1,14 @@
 <script>
 
 import iconsocial from '../../data/social';
+import products from '../../data/toprated';
 
 export default {
   name: 'Repilogue',
   data () {
     return {
         iconsocial,
-        products: [
-            {
-                title: 'Leather Gloves',
-                rate: 'fa-solid fa-star',
-                price: '$45',
-                image: 'leather_gloves-400x520.jpg'
-            },
-            {
-                title: 'Black Leather Jacket',
-                rate: 'fa-solid fa-star',
-                discount: '$235',
-                price: '$200',
-                image: 'black_elegant_leather_jacket-400x520.jpg'
-            },
-            {
-                title: 'Spring Printed Dress',
-                rate: 'fa-solid fa-star',
-                price: '$47',
-                image: 'spring_printed_dress-400x520.jpg'
-            }
-        ],    
+        products,
         list: [
             'Aenean lobortis sapien enim viverra',
             'Duis ac massa semper maximus',
---
+++ src/data/toprated.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    title: "Leather Gloves",
+    rate: "fa-solid fa-star",
+    price: "$45",
+    image: "leather_gloves-400x520.jpg",
+  },
+  {
+    title: "Black Leather Jacket",
+    rate: "fa-solid fa-star",
+    discount: "$235",
+    price: "$200",
+    image: "black_elegant_leather_jacket-400x520.jpg",
+  },
+  {
+    title: "Spring Printed Dress",
+    rate: "fa-solid fa-star",
+    price: "$47",
+    image: "spring_printed_dress-400x520.jpg",
+  },
+];
